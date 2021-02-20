19aec189fa901589d0e97cc8daa5f844dab95b76
Jeff Minnear <jeffminnear@gmail.com>
2021-02-19 17:07:32 -0700

    Turn before moving - sloppy

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -12,10 +12,11 @@ public class Player : MonoBehaviour
     private GameObject p_MovePoint;
     private NavMeshAgent p_Agent;
     private Animator p_Animator;
-
     [SerializeField]
     private float moveSpeed = 5f;
+    private float turnSpeed = 8f;
     private bool isWalking = false;
+    private Coroutine turnCoroutine;
 
     void Awake()
     {
@@ -65,17 +66,11 @@ public class Player : MonoBehaviour
                 SetWalking(false);
             }
         }
-        else
-        {
-            if (!isWalking)
-            {
-                SetWalking(true);
-            }
-        }
     }
 
     void SetWalking(bool val)
     {
+        p_Agent.isStopped = !val;
         isWalking = val;
         p_Animator.SetBool("Walk", val);
     }
@@ -93,9 +88,31 @@ public class Player : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
         {
+            SetWalking(false);
+            if (turnCoroutine != null)
+            {
+                StopCoroutine(turnCoroutine);
+            }
             p_MovePoint.transform.position = hit.point;
             p_MovePoint.SetActive(true);
             p_Agent.SetDestination(p_MovePoint.transform.position);
+            turnCoroutine = StartCoroutine(TurnTowardsMovePointAndWalk());
         }
+    }
+
+    IEnumerator TurnTowardsMovePointAndWalk()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(p_MovePoint.transform.position - transform.position);
+        targetRotation.x = transform.rotation.x;
+        targetRotation.z = transform.rotation.z;
+
+        while (Mathf.Abs(Quaternion.Dot(transform.rotation, targetRotation)) < 0.98f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.01f);
+
+        SetWalking(true);
     }
 }
